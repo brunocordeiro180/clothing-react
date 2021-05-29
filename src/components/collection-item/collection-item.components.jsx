826cbcb794fa35c5c32a133684e4761503b3ad07
Bruno Cordeiro <brunocordeiro180@gmail.com>
2021-05-28 23:54:51 -0300

    Adding multiple items to cart

--- src/components/collection-item/collection-item.components.jsx
+++ src/components/collection-item/collection-item.components.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.actions';
+
+import CustomButton from '../custom-button/custom-button.component'
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+const CollectionItem = ({ item, addItem }) => {
+	const { name, imageUrl, price} = item;
+
 	return (
 		<div className="collection-item">
 			<div
@@ -14,9 +20,14 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
 				<span className="name">{name}</span>
 				<span className="price">{price}</span>
 			</div>
+			<CustomButton onClick={() => addItem(item)} inverted>ADD TO CART</CustomButton>
 		</div>
 
 	)
 }
 
-export default CollectionItem;
+const mapDispatchToProps = dispatchEvent => ({
+	addItem: item => dispatchEvent(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
