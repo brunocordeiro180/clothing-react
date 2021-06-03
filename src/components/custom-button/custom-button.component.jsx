dc95bcea1fe0881d96c05fd4ef18d9d84738e832
Bruno Cordeiro <brunocordeiro180@gmail.com>
2021-06-02 21:11:30 -0300

    Adding styled-component to the application

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
+import { CustomButtonContainer } from './custom-button.styles';
 import './custom-button.styles.scss';
 
-const CustomButton = ( { children, isGoogleSignIn, inverted, ...otherProps}) => {
-	return(
-		<button className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button` } {...otherProps}>
+const CustomButton = ({ children, ...otherProps }) => {
+	return (
+		<CustomButtonContainer {...otherProps}>
 			{children}
-		</button>
+		</CustomButtonContainer>
 	)
 }
 
